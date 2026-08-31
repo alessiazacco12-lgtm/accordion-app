47fcf8284a49d6cffbbdc263451fc9e2924eb586
alessia <alessia.zacco12@gmail.com>
2026-08-31 14:52:07 +0200

    Add accordion to root component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Accordion } from './components/accordion/accordion';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Accordion],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('accordion-app');
-}
+export class App {}
